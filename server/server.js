5ce62fbe4c4fe939c01043ecf1e9d249239d49f5
Sujit Pal <palsujit984@gmail.com>
2026-03-05 00:37:41 +0530

    modify server.js

--- server/server.js
+++ server/server.js
@@ -200,6 +200,8 @@ app.get("/overall/:student", (req, res) => {
   );
 });
 
-app.listen(3000, () => {
-  console.log("Server running at http://localhost:3000");
+const PORT = process.env.PORT || 3000;
+
+app.listen(PORT, () => {
+  console.log("Server running on port", PORT);
 });
