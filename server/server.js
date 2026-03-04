24715f0cbab60dc8d96e686fc25d39dd02fe3716
Sujit Pal <palsujit984@gmail.com>
2026-03-05 01:05:05 +0530

    update server.js

--- server/server.js
+++ server/server.js
@@ -200,6 +200,10 @@ app.get("/overall/:student", (req, res) => {
   );
 });
 
+app.get("/", (req, res) => {
+  res.sendFile(__dirname + "/../public/index.html");
+});
+
 const PORT = process.env.PORT || 3000;
 
 app.listen(PORT, () => {
